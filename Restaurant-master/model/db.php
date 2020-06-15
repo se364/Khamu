0bf961b894c217cc783e2a282ecb60404cc5f587
Austin Parker <aparker24@mail.greenriver.edu>
2020-06-15 10:31:08 -0400

    Connected to database

--- Restaurant-master/model/db.php
+++ Restaurant-master/model/db.php
@@ -1,7 +1,7 @@
 <?php
-$database = '';
-$username = '';
-$password = '';
+$database = 'aparker1_grc';
+$username = 'aparker1';
+$password = 'AParkerDB1!';
 $hostname = 'localhost';
 
 $cnxn = @mysqli_connect($hostname, $username, $password, $database)
