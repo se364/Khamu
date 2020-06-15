f8b01a1eff86d9bfe865ce2d90ab8b25596dd6bf
Austin Parker <aparker24@mail.greenriver.edu>
2020-06-15 10:31:52 -0400

    Made menu objects

--- Restaurant-master/model/menu.php
+++ Restaurant-master/model/menu.php
@@ -1,13 +1,26 @@
 <?php
-foreach($result as $row){
-    //var_dump($row);
+class menu
+{
+    private $info;
+    private $price;
+    private $name;
+    private $image;
 
-    $firstName = $row['firstName'];
+    function __construct($name, $price, $info, $image) {
+        $this->name = $name;
+        $this->price = $price;
+        $this->info = $info;
+        $this->image = $image;
 
+    }
 
-    echo"<tr>
-         <td</td>    
-         </tr>";
+    function toString() {
+        echo "<div class=\"single-menu\">
+        <img src=\"/328/Khamu/Restaurant-master/images/food_icon0" . $this->image . ".jpg\" alt=\"food1\">
+        <div class=\"menu-content\">
+            <h4>" . $this->name . " <span> $" . $this->price . "</span></h4>
+            <p>" . $this->info . "</p>
+        </div>
+    </div>";
+    }
 }
-
-?>
