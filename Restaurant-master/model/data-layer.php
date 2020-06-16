d868301649b4f0e8a45615f3b24e04a3cc3a507d
Austin Parker <aparker24@mail.greenriver.edu>
2020-06-16 00:32:08 -0400

    Added setMenu function

--- Restaurant-master/model/data-layer.php
+++ Restaurant-master/model/data-layer.php
@@ -39,3 +39,54 @@
         $outdoor = array("Coke", "Diet Coke", "Dr.Pepper", "Fanta","Sprite","Fruit Punch","Mango Juice","Lemonade");
         return $outdoor;
     }
+
+    function setMenu($menuOption) {
+
+        require ("db.php");
+        require ("mainMenu.php");
+        require ("sideMenu.php");
+        require ("beverageMenu.php");
+
+
+        if ($menuOption == 1) {
+            $sql = "SELECT * FROM  menu";
+        }
+        elseif($menuOption == 2){
+            $sql = "SELECT * FROM  sides";
+        }
+        elseif($menuOption == 3){
+            $sql = "SELECT * FROM  beverages";
+        }
+
+        //Send the query to the db
+        $result = mysqli_query($cnxn, $sql);
+
+        //convert the query into menu objects
+        $i = 0;
+        foreach($result as $row) {
+            //var_dump($row);
+
+            $name = $row['name'];
+            $price = $row['price'];
+
+
+            if ($menuOption == "menu") {
+                $info = $row['info'];
+                $id = $row['id'];
+                $object = new mainMenu($name, $price, $info, $id);
+            }
+            elseif($menuOption == "sides"){
+                $object = new sideMenu($name, $price);
+            }
+            elseif($menuOption == "beverages"){
+                $object = new beverageMenu($name, $price);
+            }
+
+            $menuArray[$i] = $object;
+
+            $i++;
+
+        }
+
+        return $menuArray;
+    }
